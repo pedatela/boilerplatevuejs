1b144bf6591eb86dd5ece3b158f9dad7a9b592d6
pedatela <lucaspedatela@gmail.com>
2022-11-30 15:20:28 -0300

    Alterando texto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,26 +5,26 @@
     <div>
       <h4>Server Info</h4>
       <ul>
-        <li>UpTime: {{healtCheck.uptime}} segundos</li>
-        <li>Message: {{healtCheck.message}}</li>
-        <li>Date: {{healtCheck.date}}</li>
+        <li>UpTime: {{ healtCheck.uptime }} segundosasdgasgd</li>
+        <li>Message: {{ healtCheck.message }}</li>
+        <li>Date: {{ healtCheck.date }}</li>
       </ul>
     </div>
   </main>
 </template>
 
 <script>
-  import HealthCheckApi from '../services/api'
-  export default {
-    data() {
-      return {
-        healtCheck: {}
-      }
-    },
-    async mounted() {
-      console.log(process.env.VUE_APP_BACKEND_URL)
-      const {data} = await HealthCheckApi.healthcheck()
-      this.healtCheck = data
-    }
-  }
+import HealthCheckApi from "../services/api";
+export default {
+  data() {
+    return {
+      healtCheck: {},
+    };
+  },
+  async mounted() {
+    console.log(process.env.VUE_APP_BACKEND_URL);
+    const { data } = await HealthCheckApi.healthcheck();
+    this.healtCheck = data;
+  },
+};
 </script>
